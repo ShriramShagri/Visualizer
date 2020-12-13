1ed67912684f669aebd851081a86019235052892
ShriramShagri <shagrisrm@gmail.com>
2020-12-13 21:50:52 +0530

    Updated lines docs

--- Algorithms/nodes/lines.py
+++ Algorithms/nodes/lines.py
@@ -15,6 +15,12 @@ HEIGHT = 784/256
 
 class Line():
     def __init__(self, x, no):
+        """Draw lines for sorting
+
+        Args:
+            x (int): X coordinate
+            no (int): position number
+        """        
         self.value = x
         self.color = BLACK
         self.flash = 0
@@ -22,18 +28,29 @@ class Line():
         self.x = (no)*WIDTH
     
     def make_red(self):
+        """Change line color to red
+        """        
         self.color = RED
         self.flash = 1
     
     def make_green(self):
+        """Change line color to Green
+        """        
         self.color = GREEN
         self.flash = 1
     
     def reset(self):
+        """Change line color to Black
+        """        
         self.color = BLACK
         self.flash = 0
     
     def change(self, val):
+        """Change the x coordinate and position value
+
+        Args:
+            val (int): Position Value
+        """        
         self.no = val
         self.x = (val)*WIDTH
     
@@ -44,12 +61,27 @@ class Line():
         return k
     
     def evalu(self, val):
+        """set value
+
+        Args:
+            val (int): Value
+        """        
         self.value = val
     
     def posvalue(self):
+        """Get position value and number
+
+        Returns:
+            tuple: (position, no)
+        """        
         return (self.value, self.no)
 
     def draw(self, win):
+        """Draw line on specified position
+
+        Args:
+            win (pygame.Surface): Surface on which to draw
+        """        
         if self.flash > 0:
             self.flash -= 1
         elif self.flash == 0:
